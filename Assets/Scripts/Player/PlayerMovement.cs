34ef0d630ee51a6867b9abfe61d1ac2a5c0d6eb7
Andy Gates <agates01@hotmail.com>
2021-01-29 04:37:42 +0000

    Fixed merge...

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -56,8 +56,15 @@ public class PlayerMovement : MonoBehaviour
     {
         Animator = transform.GetComponent<Animator>();
         TargetPosition = MovementGrid.WorldToCell(transform.position);
-        transform.position = MovementGrid.CellToWorld(TargetPosition);
+        transform.position = OffsetCellToWorld(TargetPosition);
         Dir = EDirection.NONE;
+    }
+
+    Vector3 OffsetCellToWorld(Vector3Int CellPosition)
+    {
+        Vector3 newPos = MovementGrid.CellToWorld(TargetPosition) + new Vector3(0, (MovementGrid.cellSize.y / 2.0f), 1);
+        //Debug.Log($"{MovementGrid.cellSize.y} - {newPos}");
+        return newPos;
     }
 
     void Update()
@@ -68,9 +75,9 @@ public class PlayerMovement : MonoBehaviour
             
             if (TargetDebug)
             {
-                TargetDebug.transform.position = MovementGrid.CellToWorld(TargetPosition + IsoDir);
+                TargetDebug.transform.position = OffsetCellToWorld(TargetPosition + IsoDir);
             }
-            if (IsColliding(MovementGrid.CellToWorld(TargetPosition + IsoDir), TargetTileCollisionRadius))
+            if (IsColliding(OffsetCellToWorld(TargetPosition + IsoDir), TargetTileCollisionRadius))
             {
                 if (CurrentRotation == 180)
                     TargetRotation = 0;
@@ -99,7 +106,7 @@ public class PlayerMovement : MonoBehaviour
 
     private IEnumerator MovePlayer(Vector3Int TargetPos)
     {
-        Vector3 TargetPosWorld = MovementGrid.CellToWorld(TargetPos);
+        Vector3 TargetPosWorld = OffsetCellToWorld(TargetPos);
         OrigPos = transform.position;
 
         //if(Animator) { Animator.SetFloat("Speed", 0.2f); }
